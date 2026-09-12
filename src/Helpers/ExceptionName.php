83328fa2eb9c7120c4a2e537e5c7155c88520364
Bernd <bernd.schwendinger@gmx.de>
2026-09-12 10:29:25 +0200

    wrapper for renamed exceptions

---
+++ src/Helpers/ExceptionName.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * bschwede/wt-shared-libs: Library to share common code between webtrees custom modules
+ * 
+ * webtrees: online genealogy
+ * Copyright (C) 2026 webtrees development team
+ *                    <http://webtrees.net>
+ *
+ * Copyright (C) 2026 Bernd Schwendinger
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ * Services for class names (depending on the webtrees version)
+ *
+ */
+
+declare(strict_types=1);
+
+namespace Schwendinger\Webtrees\Helpers;
+
+use Fisharebest\Webtrees\Webtrees;
+
+
+/**
+ * Services for class names (depending on the webtrees version)
+ */
+class ExceptionName
+{
+    public const HTTP_FORBIDDEN       = 'HttpForbiddenException';
+
+
+    private const CLASS_NAMES = [
+        self::HTTP_FORBIDDEN => [
+            '2.1' =>  \Fisharebest\Webtrees\Http\Exceptions\HttpAccessDeniedException::class,
+            '2.3' =>  \Fisharebest\Webtrees\Http\Exceptions\HttpForbiddenException::class,
+        ],
+    ];
+
+    /**
+     * Get the class name (depending on the webtrees version)
+     *
+     * @param  string $name
+     *
+     * @return string
+     */
+    public static function get(string $name) : string {
+
+        if (version_compare(Webtrees::VERSION, '2.3', '>=')) {
+            return self::CLASS_NAMES[$name]['2.3'] ?? '';
+        } else {
+            return self::CLASS_NAMES[$name]['2.1'] ?? '';
+        }
+    }
+
+
+    public static function isInstanceOf(\Throwable $exception, string $name): bool
+    {
+        $class = self::get($name);        
+        return ($class !== '' && class_exists($class) && $exception instanceof $class);
+    }
+
+}
